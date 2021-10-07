035c838f5ae517668c527e62d21b7141519817ab
Kevin <kli23@stuy.edu>
2021-10-07 11:01:22 -0400

    adding files

--- strings.c
+++ strings.c
@@ -3,10 +3,26 @@
 #include <string.h>
 
 int mystrlen( char *s ) {
+	int sum = 0;
+	while (*(s != 0) {
+		sum++;
+		s++;
+	}
+	return sum;
+}
+
+char * mystrcpy( char *dest, char *source ) {
+	
+
 
 }
 
 char * mystrncpy( char *dest, char *source, int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		*(dest + i) = *(source + i); 
+	}
+	return dest;
 
 }
 
@@ -21,15 +37,61 @@ char * mystrchr( char *s, char c ) {
 }
 
 
-int main() { // testing string functions
-	char s2[100] = "hello";
-	char s3[100] = "goodbye";
-	printf("%ld\n", strlen(s2));
-	printf("%s\n", strcat(s2, s3));
-	printf("%s\n", strncat(s2, s3, 3));
-	printf("%s\n", strcpy(s2, s3));
-	char s4[100] = "sup";
-	printf("%s\n", strncpy(s2, s4,2));
+int main() {
 
+  char s1[100];
+  char s2[100] = "hello";
+  char s3[100] = "goodbye";
+  
+  printf("start strings:\n");
+  printf("s1: [%s]\n", s1);
+  printf("s2: [%s]\n", s2);
+  printf("s3: [%s]\n", s3);
+  
+  printf("\nTesting strlen(s2):\n");
+  printf("[standard]:\t%lu\n", strlen(s2) );
+  printf("[mine]:\t\t%d\n", mystrlen(s2) );
+	/*
+  printf("\nTesting strcpy(s1, s2):\n");
+  printf("[standard]:\t[%s]\n", strcpy(s1, s2) );
+  printf("[mine]:\t\t[%s]\n", mystrcpy(s1, s2) );
+*/
+  printf("\nTesting strncpy(s1, s3, 3):\n");
+  printf("[standard]:\t[%s]\n", strncpy(s1, s3, 3) );
+  printf("[mine]:\t\t[%s]\n", mystrncpy(s1, s3, 3) );
+  /*
+  printf("\nTesting strcat(s1, s3):\n");
+  printf("[standard]:\t[%s]\n", strcat(s1, s3) );
+  s1[5] = 0;
+  printf("[mine]:\t\t[%s]\n", mystrcat(s1, s3) );
 
+  printf("\nTesting strncat(s1, s2, 3):\n");
+  printf("[standard]:\t[%s]\n", strncat(s1, s2, 3) );
+  s1[12] = 0;
+  printf("[mine]:\t\t[%s]\n", mystrncat(s1, s2, 3) );
+
+  printf("\nTesting strchr(s1, 'l'):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 'l') );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 'l') );
+  
+  printf("\nTesting strchr(s1, 0):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 0) );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 0) );
+
+  printf("\nTesting strchr(s1, 'z'):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 'z') );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 'z') );
+
+  printf("\nTesting strcmp\n");
+  printf("\tComparting ab to abc:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("ab", "abc") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("ab", "abc") );
+  printf("\tComparting abc to ab:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("abc", "ab") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("abc", "ab") );
+  printf("\tComparting abc to abc:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("abc", "abc") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("abc", "abc") );
+*/
+  return 0; 
 }
